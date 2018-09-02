3dd6fcf83bdbb8639f5eb527668d5cd0ae367df3
lizu <535733495@qq.com>
2018-09-02 21:44:04 +0800

    新增注释和业务逻辑

--- core/src/main/java/com/fayayo/job/core/spi/impl/ExecutorSpiImpl.java
+++ core/src/main/java/com/fayayo/job/core/spi/impl/ExecutorSpiImpl.java
@@ -25,6 +25,7 @@ public class ExecutorSpiImpl implements ExecutorSpi{
         log.info("{}start to run job......params:{}", Constants.LOG_PREFIX,jobInfo.toString());
         String type=jobInfo.getJobType();
 
+        //TODO 现有任务类型作用不大（修改:基于注解bean模式  和 其他模式  表新增一个执行器类型(DATAX,SPARK可以区分)）
         if(type.equalsIgnoreCase(JobTypeEnums.DATAX.getName())){
             //获取具体执行的服务
             JobExecutorHandler handler=JobExecutor.getHandler();
